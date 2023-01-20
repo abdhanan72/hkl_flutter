f6c43e4c0795e65ff8b378a71cafd2e68b1f1208
abdhanan72 <abdhananvattoli@gmail.com>
2023-01-20 23:52:03 +0530

    almost

--- lib/salesReport/sales_report.dart
+++ lib/salesReport/sales_report.dart
@@ -41,94 +41,91 @@ class _SalesReportState extends State<SalesReport> {
       child: SafeArea(
         child: Scaffold(
           body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Column(
-                children: [
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: TextField(
-                      controller: fromdate,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12)),
-                          prefixIcon: const Icon(Icons.calendar_today_outlined),
-                          labelText: 'From Date'),
-                      readOnly: true,
-                      onTap: () async {
-                        DateTime? pickdate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(2000),
-                            lastDate: DateTime(2100));
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: TextField(
+                  controller: fromdate,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      prefixIcon: const Icon(Icons.calendar_today_outlined),
+                      labelText: 'From Date'),
+                  readOnly: true,
+                  onTap: () async {
+                    DateTime? pickdate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100));
 
-                        if (pickdate != null) {
-                          String formateddate =
-                              DateFormat("dd-MM-yyyy").format(pickdate);
-                          setState(() {
-                            fromdate.text = formateddate.toString();
-                          });
-                        } else {}
-                      },
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: TextField(
-                      controller: todate,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12)),
-                          prefixIcon: const Icon(Icons.calendar_today_outlined),
-                          labelText: 'To Date'),
-                      readOnly: true,
-                      onTap: () async {
-                        DateTime? pickeddate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(2000),
-                            lastDate: DateTime(2100));
+                    if (pickdate != null) {
+                      String formateddate =
+                          DateFormat("dd-MM-yyyy").format(pickdate);
+                      setState(() {
+                        fromdate.text = formateddate.toString();
+                      });
+                    } else {}
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: TextField(
+                  controller: todate,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      prefixIcon: const Icon(Icons.calendar_today_outlined),
+                      labelText: 'To Date'),
+                  readOnly: true,
+                  onTap: () async {
+                    DateTime? pickeddate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100));
 
-                        if (pickeddate != null) {
-                          String formatdate =
-                              DateFormat("dd-MM-yyyy").format(pickeddate);
-                          setState(() {
-                            todate.text = formatdate.toString();
-                          });
-                        } else {
-                          print('Invalid date');
-                        }
-                      },
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                      onPressed: () async {
-                        final result = await fetchdata(
-                            fid: firmId.toString(),
-                            frdt: fromdate.text,
-                            todt: todate.text,
-                            t: ti);
+                    if (pickeddate != null) {
+                      String formatdate =
+                          DateFormat("dd-MM-yyyy").format(pickeddate);
+                      setState(() {
+                        todate.text = formatdate.toString();
+                      });
+                    } else {
+                      print('Invalid date');
+                    }
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    final result = await fetchdata(
+                        fid: firmId.toString(),
+                        frdt: fromdate.text,
+                        todt: todate.text,
+                        t: ti);
 
-                            
-                      },
-                      child: const Text('Generate')),
+                        
+                  },
+                  child: const Text('Generate')),
 
 
-                      
-                 FutureBuilder<Reportsales>(
+                  
+             FutureBuilder<Reportsales>(
    future: fetchdata(fid: firmId.toString(),
-                            frdt: fromdate.text,
-                            todt: todate.text,
-                            t: ti),
+                        frdt: fromdate.text,
+                        todt: todate.text,
+                        t: ti),
    builder: (BuildContext context, AsyncSnapshot<Reportsales> snapshot) {
      if (snapshot.hasData) {
        final data = snapshot.data?.data;
@@ -137,16 +134,16 @@ class _SalesReportState extends State<SalesReport> {
            child: SizedBox(
             height: 200,
              child: ListView.builder(
-              itemCount: data!.length,
-              itemBuilder: (BuildContext context, int index) {
-                final Datum datum = data[index];
-                return ListTile(
-                  title: Text(datum.the0),
-                  subtitle: Text(datum.the1),
-                  trailing: Text(datum.custName),
-                );
-              },
-                   ),
+          itemCount: data!.length,
+          itemBuilder: (BuildContext context, int index) {
+            final Datum datum = data[index];
+            return ListTile(
+              title: Text(datum.the0),
+              subtitle: Text(datum.the1),
+              trailing: Text(datum.custName),
+            );
+          },
+               ),
            ),
          );
        
@@ -157,8 +154,6 @@ class _SalesReportState extends State<SalesReport> {
    },
  ),
 
-                ],
-              )
             ],
           ),
         ),
